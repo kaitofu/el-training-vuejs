3b104642cc81405201387ed3441de0aac4c8af46
koalamask <yaiag04@gmail.com>
2019-02-04 15:49:32 +0900

    Fix layout router-view layout doesn't hide below header #14

--- app/javascript/packs/components/Header.vue
+++ app/javascript/packs/components/Header.vue
@@ -1,37 +1,29 @@
 <template lang="pug">
 div
-    v-toolbar(color='orange darken-4', dark='', fixed='', app='')
-      v-toolbar-title El-Training
-      v-spacer
-      v-toolbar-side-icon(@click.stop='drawer = !drawer')
-
-    v-navigation-drawer(fixed='', v-model='drawer', right='', app='')
-      v-list(dense='')
-        v-list-tile(@click='')
-          v-list-tile-action
-            v-icon home
-          v-list-tile-content
-            v-list-tile-title Home
-        v-list-tile(@click='')
-          v-list-tile-action
-            v-icon contact_mail
-          v-list-tile-content
-            v-list-tile-title Contact
-    //- v-content
-    //-   v-container(fluid='', fill-height='')
-    //-     v-layout(justify-center='', align-center='', row wrap)
-    //-       //- v-flex(text-xps-center='')
+  v-toolbar(color='orange darken-4', dark='', fixed='', app='')
+    v-toolbar-title El-Training
+    v-spacer
+    router-link(to="/") Home 
+    router-link(to="/login") Login
+    router-link(to="/admin") Admin
+    v-btn(v-if="this.$store.getters.loggedIn" @click="logout()") logout
 </template>
 
 <script>
 export default {
-  data: function () {
+  data() {
     return {
-      drawer: false
     }
   },
   props: {
     source: String
+  },
+  methods: {
+    logout() {
+    this.$store.dispatch('SetLogOut')
+    this.$store.dispatch('SetAuthToken', '')
+    this.$router.push('/login')
+    }
   }
 }
 </script>
